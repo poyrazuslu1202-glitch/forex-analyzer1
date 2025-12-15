26ab34fbd15c00147f5bde53c79867fcfdebb7d4
poyrazuslu1202-glitch <poyrazuslu1202@gmail.com>
2025-12-16 02:57:38 +0300

    Fix Vercel config for static Flutter build

--- app/build/web/flutter_bootstrap.js
+++ app/build/web/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"a18df97ca57a249df5d8d68cd0820600223ce2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "821839747"
+    serviceWorkerVersion: "4066305692"
   }
 });
